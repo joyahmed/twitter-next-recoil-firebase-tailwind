e67480ce8271fe7b4771efd2f3f4ddab20de951c
joyahmed <joy_ahmed_007@yahoo.com>
2021-12-06 02:47:08 +0600

    fron

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	reactStrictMode: true,
+	webpack: config => {
+		config.resolve.alias['@'] = path.resolve(__dirname);
+		return config;
+	},
+	images: {
+		domains: ['rb.gy']
+	}
+};
