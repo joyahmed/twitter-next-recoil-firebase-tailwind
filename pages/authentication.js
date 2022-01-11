3ab06d13117954033eb861e9de67966bd9295ca9
Gulnaz Valiullina <gulnazmoon91@gmail.com>
2022-01-11 15:02:16 +0300

    autentication page

---
+++ pages/authentication.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const authentication = () => {
+    return (
+        <div className='bg inline-flex'>
+            <div className='max-w-2xl h-full flex items-center justify-center xl:px-0'>
+                <img src="https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png" alt="" />
+            </div>
+            <div className='mt-6'>
+                <h1 className='ml-10 text-[#d9d9d9] text-6xl'>Follow your interests</h1>
+                <h1 className='ml-10 text-4xl text-white'>Join Twitter</h1>
+            </div>
+        </div>
+    )
+}
+
+export default authentication
